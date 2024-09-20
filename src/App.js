cae12343bdcd46e9204df686dacbe849fbd82c35
Rohit <rohhit.rz@gmail.com>
2024-09-20 23:22:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@
 // import DebouncedInput from "./components/LodashDebouncing";
 import UseLayout from "./components/useLayoutEffect";
 import "./App.css"
+import { ThemeProvider,useTheme } from "./components/usecontexthook";
 
+
+const ThemeSwitcher = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div style={{ background: theme === 'light' ? '#fff' : '#333', color: theme === 'light' ? '#000' : '#fff', padding: '20px' }}>
+      <h1>Current Theme: {theme}</h1>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       {/* <DebouncedInput/> */}
-      <UseLayout/>
+      {/* <UseLayout/> */}
+      <ThemeProvider>
+      <ThemeSwitcher />
+    </ThemeProvider>
 
       
     </div>
